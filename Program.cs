00969330e1db0eee930c268203863edf4a5947cd
Mattias Kindborg <FantasticFiasco@users.noreply.github.com>
2014-01-13 07:06:53 +0000

    Initial commit. Added log4net as NuGet, have not configured it yet.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.ServiceProcess;
+
+namespace DynDnsClient
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            var servicesToRun = new ServiceBase[] 
+            { 
+                new Service() 
+            };
+
+            ServiceBase.Run(servicesToRun);
+        }
+    }
+}
