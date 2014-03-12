50c4899838e89d09924f1710eaf396c10c20da74
Mattias Kindborg <FantasticFiasco@users.noreply.github.com>
2014-03-12 21:06:30 +0000

    Added 'www.code' as host.

--- DynDnsClient/Properties/Settings.Designer.cs
+++ DynDnsClient/Properties/Settings.Designer.cs
@@ -55,10 +55,13 @@ namespace DynDnsClient.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsi=\"http://www.w3." +
-            "org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\r\n  <s" +
-            "tring>@</string>\r\n  <string>www</string>\r\n  <string>www.myphotos</string>\r\n</Arr" +
-            "ayOfString>")]
+        [global::System.Configuration.DefaultSettingValueAttribute(@"<?xml version=""1.0"" encoding=""utf-16""?>
+<ArrayOfString xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"">
+  <string>@</string>
+  <string>www</string>
+  <string>www.code</string>
+  <string>www.myphotos</string>
+</ArrayOfString>")]
         public global::System.Collections.Specialized.StringCollection Hosts {
             get {
                 return ((global::System.Collections.Specialized.StringCollection)(this["Hosts"]));
