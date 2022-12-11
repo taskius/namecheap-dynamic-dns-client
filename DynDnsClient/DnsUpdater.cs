4c6abf6cffe34831c532cd3755605500b1dd5426
Darius <darius@taskius.com>
2022-12-11 16:19:34 +0000

    improve error message when updating dns

--- DynDnsClient/DnsUpdater.cs
+++ DynDnsClient/DnsUpdater.cs
@@ -1,17 +1,26 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Reflection;
-using log4net;
+using System.Text.RegularExpressions;
 
 namespace DynDnsClient
 {
     internal class DnsUpdater
     {
         private const string Url = "https://dynamicdns.park-your-domain.com/update?host={0}&domain={1}&password={2}";
-        
+
         private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
-        
+
+        public DnsUpdater()
+        {
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+        }
+
         public DnsUpdateResult Update(string domain, IEnumerable<string> hosts, string password)
         {
             var result = new DnsUpdateResult();
@@ -34,7 +43,28 @@ namespace DynDnsClient
                 try
                 {
                     string response = client.DownloadString(url);
-                    return response.Contains("<ErrCount>0</ErrCount>");
+                    if (response.Contains("<ErrCount>0</ErrCount>"))
+                    {
+                        return true;
+                    }
+
+                    var regex = new Regex("<Description>(.*)</Description>");
+                    var match = regex.Match(response);
+                    if (match.Success)
+                    {
+                        var message = match.Value;
+
+                        if (match.Groups.Count == 2)
+                        {
+                            message = match.Groups[1].Value;
+                        }
+
+                        throw new Exception("Failed to update dns:\nError Description: " + message);
+                    }
+
+                    // If there's no description then just log debug for now. logs will still show an error updating the dns
+                    Log.Debug(response);
+                    return false;
                 }
                 catch (Exception e)
                 {
